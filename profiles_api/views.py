2afc16bf34bfa1121e7079cb1f9a5e80c4aba84e
sparshsaxenain <sparshsaxena3649@gmail.com>
2023-07-07 00:50:34 +0530

    Completed Course

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -9,6 +9,7 @@ from profiles_api import permissions
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 
 class HelloApiView(APIView):
@@ -143,6 +144,12 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticated,
+    )
     
     def perform_create(self, serializer):
         serializer.save(user_profile = self.request.user) ##lecture number 63
+
+    
